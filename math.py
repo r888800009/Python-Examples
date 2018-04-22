9154d85a1e0950bcc4e76219561f0e1a1b346b5f
r888800009 <r888800009@users.noreply.github.com>
2018-04-23 07:58:25 +0800

    add math.py

---
+++ math.py
@@ -0,0 +1,30 @@
+
+#Power
+print("sqrt")
+print(2**0.5)
+print(4**0.5)
+print(8**0.5)
+print(16**0.5)
+
+print("square of 2")
+print(3**2)
+
+
+#quotient
+print(20//7)    
+print(10//3)
+
+
+
+
+
+print("師傅有" , 100-25*3%4 ,"元")
+print(3+2<5-7)
+     
+
+
+seafood_money = 6666
+my_money = 1
+total = seafood_money+my_money
+print(total)
+
