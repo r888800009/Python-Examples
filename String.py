a3dc636bdcc87e08a319997fb9d29538024fb4e4
r888800009 <r888800009@users.noreply.github.com>
2018-04-23 07:59:08 +0800

    update String.py

--- String.py
+++ String.py
@@ -14,3 +14,30 @@ print(c0+c1+c2,end=" 123")
 ##end 可以設定print 的結尾字元或字串
 print(word)
 
+
+
+
+
+
+
+
+aaa="\t aaa"
+print(aaa)
+##同"""長文書出
+print('''
+\t"Hello",
+\t"My name is Lin"
+\t"hey hey"
+''')
+print("\\123\\")
+## \t \' \" \f
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months ="Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+print("days: ",days)
+print("months: ", months)
+##長文輸出
+print("""
+Hello,
+My name is Lin
+""")
