dd4c6da906a8cc94e2b307fdb82595c38de9bc5f
r888800009 <r888800009@users.noreply.github.com>
2018-04-23 07:58:13 +0800

    add input.py

---
+++ IO/input.py
@@ -0,0 +1,16 @@
+print("你多老")
+age =input()
+print("How tall are you?")
+height=input()
+print("How much do you weight?")
+weight=input()
+#python 3.6
+##print(f"{age},{height},{weight}")
+print("{2},{1},{0}".format(age,height,weight))
+
+print("old:",end="")
+old=input()
+print(old)
+#see also
+#   /String.py
+#   /Formatter.py
